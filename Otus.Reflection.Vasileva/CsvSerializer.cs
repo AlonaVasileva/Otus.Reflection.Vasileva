ef45d376205f48023f1186f08ce6386f28ee280f
AlonaVasileva <vsemposhapke@gmail.com>
2024-10-21 13:48:33 +0300

    Добавьте файлы проекта.

---
+++ Otus.Reflection.Vasileva/CsvSerializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+public class CsvSerializer
+{
+    public static string Serialize<T>(T obj)
+    {
+        StringBuilder csv = new StringBuilder();
+        PropertyInfo[] properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var prop in properties)
+        {
+            csv.Append(prop.GetValue(obj));
+            csv.Append(",");
+        }
+        return csv.ToString().TrimEnd(',');
+    }
+
+    public static T Deserialize<T>(string csv) where T : new()
+    {
+        T obj = new T();
+        string[] values = csv.Split(',');
+        PropertyInfo[] properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        for (int i = 0; i < properties.Length && i < values.Length; i++)
+        {
+            properties[i].SetValue(obj, Convert.ChangeType(values[i], properties[i].PropertyType));
+        }
+        return obj;
+    }
+}
---
+++ Otus.Reflection.Vasileva/Program.cs
@@ -0,0 +1,33 @@
+﻿using Otus.Reflection.Vasileva;
+using System;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main()
+    {
+        int iterations = 1000;
+        F obj = F.Get();
+
+        // Сериализация с использованием Newtonsoft.Json
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        for (int i = 0; i < iterations; i++)
+        {
+            string json = JsonConvert.SerializeObject(obj);
+        }
+        stopwatch.Stop();
+        Console.WriteLine($"Время сериализации JSON: {stopwatch.ElapsedMilliseconds} ms");
+
+        // Десериализация с использованием Newtonsoft.Json
+        string jsonString = JsonConvert.SerializeObject(obj);
+        stopwatch.Restart();
+        for (int i = 0; i < iterations; i++)
+        {
+            F newObj = JsonConvert.DeserializeObject<F>(jsonString);
+        }
+        stopwatch.Stop();
+        Console.WriteLine($"Время десериализации JSON: {stopwatch.ElapsedMilliseconds} ms");
+        Console.ReadKey();
+    }
+}
